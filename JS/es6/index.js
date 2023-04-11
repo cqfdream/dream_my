ea56788cc49e94cf2d7c42b908768549b0d42ce9
cqfdream <2510652284@qq.com>
2023-04-11 16:39:00 +0800

    es6

---
+++ JS/es6/index.js
@@ -0,0 +1,37 @@
+// var tmp = 123
+// if (true){
+//   let tmp 
+//   tmp ='123'
+// }
+
+
+// let a = 1
+// let a = 2
+// console.log(a);
+
+
+// var tmp = new Date()
+// function f(){
+//   console.log(tmp);
+//   if (false){
+//     let tmp = 'hello world'
+//   }
+// }
+// f()
+
+
+
+
+// const key = 'abc'
+// let obj = {}
+// obj [key]=123
+// console.log(obj);
+
+
+
+
+// const obj ={
+//   name: 'zj'
+// }
+// obj.name = 'ls'
+// console.log(obj);
